e77e7c88e4cf00a8135300d2686d77244b711ffa
wizsenay <tomasfrehiwot@gmail.com>
2024-10-14 21:29:33 +0300

    update

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,26 @@
+#!/usr/bin/env python3
+"""
+0-basic_async_syntax.py
+
+ this modules is an asynchronous coroutine that takes in an integer argument
+... and waits for a random delay between 0 and the given integer seconds and
+... eventually returns it
+
+Functions:
+    wait_random = wate to a randem number and return it
+"""
 import asyncio
 import random
-async def wait_random(max_delay = 10):
+
+
+async def wait_random(max_delay=10):
+    """
+        wait tile to 'delay' secound
+    Arg:
+       max_delay: the maximum time to stop the program from ranning
+    Return:
+       how many secound it stops
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
